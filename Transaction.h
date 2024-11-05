9317b423d49cbc963f55f93a4d5f6f046fce8003
miahweems <142929323+miahweems@users.noreply.github.com>
2024-11-04 21:35:08 -0600

    Create Transaction.h

---
+++ Transaction.h
@@ -0,0 +1,15 @@
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+#include <string>
+#include <vector>
+
+struct Transaction {
+    std::string transactionID;         // Unique ID for each transaction
+    std::string userID;                // ID of the user making the transaction
+    std::vector<std::string> productIDs; // List of product IDs purchased
+    float totalAmount = 0.0f;          // Total amount of the transaction
+    int rewardPoints = 0;              // Reward points earned from the transaction
+};
+
+#endif
